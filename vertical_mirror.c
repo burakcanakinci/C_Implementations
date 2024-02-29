7e1f0a8dce8fe9390aa1b261de264c97fcbb2fd5
burakcanakinci <burakakinci.bca@gmail.com>
2024-02-29 17:23:40 +0200

    vertical_mirror update

--- vertical_mirror.c
+++ vertical_mirror.c
@@ -6,39 +6,34 @@
 /* after VMirror execution : 0xFE,0x02,0x02,0x02,0xFE,0x00,0x00,0x00  */
 
 #include <stdio.h>
-void VMirror(unsigned char *pImage) {
-    // Reverse the order of the rows
-    
-    for (int i = 0; i < 4; i++) {
-        unsigned char temp = pImage[i];
-        pImage[i] = pImage[7 - i];
-        pImage[7 - i] = temp;
-    }
 
-    // Reverse the bits within each row
-    for (int i = 0; i < 8; i++) {
-        pImage[i] = (pImage[i] >> 4) | (pImage[i] << 4);
-        pImage[i] = ((pImage[i] & 0xCC) >> 2) | ((pImage[i] & 0x33) << 2);
-        pImage[i] = ((pImage[i] & 0xAA) >> 1) | ((pImage[i] & 0x55) << 1);
+void VMirror(unsigned char *pImage) {
+    for (int i = 0; i < 8; ++i) {
+        unsigned char row = pImage[i];
+        unsigned char mirroredRow = 0;
+
+        for (int j = 0; j < 8; ++j) {
+            mirroredRow |= ((row >> j) & 0x01) << (7 - j);
+        }
+
+        pImage[i] = mirroredRow;
     }
 }
 
 int main() {
     unsigned char image[] = {0x7F, 0x40, 0x40, 0x40, 0x7F, 0x00, 0x00, 0x00};
-    
-    printf("Original image:\n");
-    for (int i = 0; i < 8; i++) {
-        printf("%02X", image[i]);
+
+    printf("Initial Content: ");
+    for (int i = 0; i < 8; ++i) {
+        printf("0x%02X ", image[i]);
     }
-    printf("\n");
 
     VMirror(image);
 
-    printf("Vertically mirrored image:\n");
-    for (int i = 0; i < 8; i++) {
-        printf("%02X ", image[i]);
+    printf("\nAfter VMirror Execution: ");
+    for (int i = 0; i < 8; ++i) {
+        printf("0x%02X ", image[i]);
     }
-    printf("\n");
 
     return 0;
 }
