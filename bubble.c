3812e2a66ddb678c705c07ac62aeda107c4a7b94
burakcanakinci <burakakinci.bca@gmail.com>
2024-02-29 17:41:43 +0200

    bubble sort update

--- bubble.c
+++ bubble.c
@@ -2,51 +2,44 @@
 /* unsigned int uLen)”, where pArary is a pointer to unsorted array and uLen is its size.  */
 
 #include <stdio.h>
-#include <stdbool.h>
-void Bubble(int* pArray, unsigned int uLen) {
-    int i, j;
-    bool swapped;
 
-    for (i = 0; i < uLen - 1; i++) {
-        swapped = false;
-        printf("Swapped false\n");
-        for (j = 0; j < uLen - i - 1; j++) {
+void Bubble(int* pArray, unsigned int uLen) {
+    for (unsigned int i = 0; i < uLen - 1; ++i) {
+        for (unsigned int j = 0; j < uLen - i - 1; ++j) {
             if (pArray[j] > pArray[j + 1]) {
                 int temp = pArray[j];
                 pArray[j] = pArray[j + 1];
                 pArray[j + 1] = temp;
-                printf("Swapped true\n");
-                swapped = true;
             }
-        }
-        if (!swapped) {
-            break;
         }
     }
 }
 
 int main() {
-    unsigned int uLen;
+
+    unsigned int arraySize;
     printf("Enter the size of the array: ");
-    scanf("%u", &uLen);
+    scanf("%u", &arraySize);
 
-    int array[uLen];
-    printf("Enter the elements of the array:\n");
-    for (int i = 0; i < uLen; i++) {
-        scanf("%d", &array[i]);
+    int unsortedArray[arraySize];
+
+    printf("Enter %u elements of the array:\n", arraySize);
+    for (unsigned int i = 0; i < arraySize; ++i) {
+        scanf("%d", &unsortedArray[i]);
     }
 
-    printf("\nUnsorted Array:\n");
-    for (int i = 0; i < uLen; i++) {
-        printf("%d, ", array[i]);
+    printf("Unsorted Array: ");
+    for (unsigned int i = 0; i < arraySize; ++i) {
+        printf("%d ", unsortedArray[i]);
+    }
+
+    Bubble(unsortedArray, arraySize);
+
+    printf("\nSorted Array: ");
+    for (unsigned int i = 0; i < arraySize; ++i) {
+        printf("%d ", unsortedArray[i]);
     }
     printf("\n");
 
-    Bubble(array, uLen);
-    printf("\nSorted Array:\n");
-    for (int i = 0; i < uLen; i++) {
-        printf("%d, ", array[i]);
-    }
-    printf("\n");
     return 0;
 }
