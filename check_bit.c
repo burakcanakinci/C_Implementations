3c0b1dec3b451b18c73d0d0f9a5fa16927c57487
burakcanakinci <burakakinci.bca@gmail.com>
2024-02-29 11:09:20 +0200

    CheckBit problem

--- check_bit.c
+++ check_bit.c
@@ -7,4 +7,22 @@
 /* Input = 0x0600, Output = 0x00 */
 /* Input = 0x0000, Output = 0x00  */
 
+#include <stdio.h>
+#include <stdbool.h>
 
+unsigned char CheckBit(unsigned int uValue) {
+    bool oneBitIsSet = (uValue != 0) && ! (uValue & (uValue - 1));
+    return oneBitIsSet ? '1' : '0';
+}
+
+int main() {
+    unsigned int uValue;
+
+    printf("Enter a 16-bit unsigned integer: ");
+    scanf("%u", &uValue);
+
+    unsigned char result = CheckBit(uValue);
+    printf("Result: %c\n", result);
+
+    return 0;
+}
